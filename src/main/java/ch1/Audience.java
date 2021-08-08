7b82cd9769d6f0c7038b2753b4d9c5084fb5883b
you-shin-il <kyoyaiida@naver.com>
2021-08-08 15:14:07 +0900

    Initial commit

---
+++ src/main/java/ch1/Audience.java
@@ -0,0 +1,17 @@
+package ch1;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+
+    public long buy(Ticket ticket) {
+        return bag.hold(ticket);
+    }
+}
---
+++ src/main/java/ch1/Bag.java
@@ -0,0 +1,39 @@
+package ch1;
+
+public class Bag {
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public Bag(Long amount) {
+        this(null, amount);
+    }
+
+    public Bag(Invitation invitation, Long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
+
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+}
